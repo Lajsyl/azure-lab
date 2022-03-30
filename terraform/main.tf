550798e6b712d7ddc5803ed26425550823ee01d8
lisa-karlsson-wcar <lisa.karlsson@wirelesscar.com>
2022-03-30 14:52:21 +0200

    update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,26 +23,25 @@ resource "azurerm_resource_group" "this" {
 }
 
 resource "azurerm_app_service_plan" "this" {
-    name = local.app_service_plan_name
-    location = azurerm_resource_group.this.location
-    resource_group_name = azurerm_resource_group.this.name
-    kind = "Linux"
-    reserved = true
+  name                = local.app_service_plan_name
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  kind                = "Linux"
+  reserved            = true
 
-    sku {
-      tier = "Basic"
-      size = "B1"
-    }
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
 }
 
 resource "azurerm_app_service" "this" {
-    name = local.app_service_name
-    location = azurerm_resource_group.this.location
-    resource_group_name = azurerm_resource_group.this.name
-    app_service_plan_id = azurerm_app_service_plan.this.id
+  name                = local.app_service_name
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  app_service_plan_id = azurerm_app_service_plan.this.id
 
-    site_config {
-      linux_fx_version = "NODE|16-lts"
-    }
-  
+  site_config {
+    linux_fx_version = "NODE|16-lts"
+  }
 }
